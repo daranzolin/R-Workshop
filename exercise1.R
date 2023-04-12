5582db2473f2c15422ea62bcfa73dbbd7799e602
David Ranzolin <daranzolin@gmail.com>
2023-04-12 16:48:14 -0700

    wfc_codes and exercise

---
+++ exercise1.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(lubridate)
+
+exits <- read_csv()
+
+# Filter all rows where:
+#   Agency Code is 158
+#   Job Start Date is after July 1, 2022
+#   Employment Industry is not NA
+# Then select Application Number, Employer Name, Employer City, Job Title,
+# Hours Per Week, and Hourly Wage,
+# Assign the output to a data frame called self_help
+
+
+# How many of these exits received jobs *not* in California?
+
+# What is the most popular Job Title? Who is the most popular employer?
+
+# Calculate the minimum, mean, median, and maximum Hourly Wage
+
+
+# Calculate the minimum, mean, median, and maximum Hourly Wage
+# for each employer
+
+
+
